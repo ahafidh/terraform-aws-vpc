238f41e82ce3a2a99b181320898d1b3f5db52542
Abdullah Hafidh <abdullah.hafidh@traveloka.com>
2020-09-09 20:19:04 +0700

    support v0.13.2

--- variables.tf
+++ variables.tf
@@ -1,41 +1,41 @@
 variable "product_domain" {
   description = "Product domain these resources belong to."
-  type        = "string"
+  type        = string
 }
 
 variable "environment" {
   description = "Type of environment these resources belong to."
-  type        = "string"
+  type        = string
 }
 
 variable "vpc_name" {
   description = "The name of the VPC. This name will be used as the prefix for all VPC components."
-  type        = "string"
+  type        = string
 }
 
 variable "vpc_cidr_block" {
   description = "The CIDR block for the VPC."
-  type        = "string"
+  type        = string
 }
 
 variable "vpc_enable_dns_support" {
   description = "A boolean flag to enable/disable DNS support in the VPC. Defaults true."
-  default     = "true"
+  default     = true
 }
 
 variable "vpc_enable_dns_hostnames" {
   description = "A boolean flag to enable/disable DNS hostnames in the VPC. Defaults true."
-  default     = "true"
+  default     = true
 }
 
 variable "vpc_multi_tier" {
   description = "Whether this VPC should have 3 tiers. True means 3-tier, false means single-tier. Defaults true. Recommended value is true."
-  default     = "true"
+  default     = true
 }
 
 variable "subnet_availability_zones" {
   description = "List of AZs to spread VPC subnets over."
-  type        = "list"
+  type        = list
 
   default = [
     "ap-southeast-1a",
@@ -86,5 +86,5 @@ variable "flowlogs_bucket_retention_in_days" {
 
 variable "flowlogs_s3_logging_bucket_name" {
   description = "S3 bucket name to store FlowLogs S3 Bucket log"
-  type        = "string"
+  type        = string
 }
